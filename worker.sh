a98a75ef1ab5da77a6ac19bbd8707a0ea7b95283
Alissonmaia <alissonmaiadasilva28@gmail.com>
2023-03-22 00:08:26 -0300

    Primeiro commit

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-19uba6nz84p7i2yt9duaj0bkcp0rel5gsx6iop1zt672yn1p59-80dn7ms3jgw570skvmnczhtgy 10.10.10.100:2377
